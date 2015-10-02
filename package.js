d4cfe75ff0e026236e7511834930506d45411db5
Mike Kornelson <mike@koretech.ca>
2015-10-02 11:23:18 -0500

    Updated package versions. Updated to Meteor 1.2

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'krt:elasticsearch',
   summary: "Sync collections with ElasticSearch and search them. Heavy influence from easy-search",
-  version: "0.2.1",
+  version: "0.2.2",
   documentation: null
 });
 
 Npm.depends({
-  'elasticsearch': '2.2.0'
+  'elasticsearch': '8.2.0'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0');
+  api.versionsFrom('METEOR@1.2');
 
   api.use([
     'underscore',
@@ -18,7 +18,7 @@ Package.onUse(function(api) {
     'mongo-livedata',
     'meteor',
     'meteor-platform',
-    'krt:core@0.1.3'
+    'krt:core@0.1.4'
   ], ['client', 'server']);
 
   api.use([
